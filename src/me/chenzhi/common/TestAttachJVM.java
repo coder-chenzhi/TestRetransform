53dc052a2196c89e55037cfcb1d4472b93355199
coder-chenzhi <zilingqishi@gmail.com>
2016-05-26 23:42:08 +0800

    format code

---
+++ src/me/chenzhi/common/TestAttachJVM.java
@@ -0,0 +1,20 @@
+package me.chenzhi.common;
+
+import java.lang.management.ManagementFactory;
+
+import com.sun.tools.attach.VirtualMachine;
+
+public class TestAttachJVM {
+    public static void main(String[] args) {
+	try {
+	    String agentPath = "AllLoadedClassAgent.jar";
+	    String pid = ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
+	    // Attach (to ourselves)
+	    VirtualMachine vm = VirtualMachine.attach(pid);
+	    vm.loadAgent(agentPath);
+	} catch (Exception e) {
+	    System.err.println("Agent Installation Failed. Stack trace follows...");
+	    e.printStackTrace(System.err);
+	}
+    }
+}
